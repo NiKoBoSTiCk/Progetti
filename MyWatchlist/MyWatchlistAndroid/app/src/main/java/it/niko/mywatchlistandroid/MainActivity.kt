3c2b3f218b7509d1d2bd4a915eb9d9e2845c31f5
NiKoBoSTiCk <salvatore.romanello@hotmail.it>
2022-04-05 17:23:07 +0200

    initial commit

--- MyWatchlist/MyWatchlistAndroid/app/src/main/java/it/niko/mywatchlistandroid/MainActivity.kt
+++ MyWatchlist/MyWatchlistAndroid/app/src/main/java/it/niko/mywatchlistandroid/MainActivity.kt
@@ -2,26 +2,10 @@ package it.niko.mywatchlistandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import it.niko.mywatchlistandroid.model.Series
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      //  setSupportActionBar(toolbar)
-
-        val series = arrayListOf<Series>()
-
-        for(i in 0..100){
-            val genres = arrayListOf<String>()
-            genres.add("DRAMA")
-            series.add(Series("Breaking Bad","" , 80, "nice", genres))
-        }
-
-        recycler_view.apply {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = SeriesAdapter()
-        }
     }
 }
--- MyWatchlist/MyWatchlistAndroid/app/src/main/java/it/niko/mywatchlistandroid/model/Series.kt
+++
@@ -1,9 +0,0 @@
-package it.niko.mywatchlistandroid.model
-
-data class Series(
-    val title: String,
-    val photoUrl: String,
-    val episodes: Int,
-    val plot: String,
-    val genres: ArrayList<String>
-)
